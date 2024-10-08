cefbd4974220aa4fdbd21dd73ee0213b0d3bc9a1
Nick Ververis <nikolaos.ververis@sword-group.com>
2024-10-08 23:28:40 +0300

    Initial Commit

---
+++ Better/BetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterApp.swift
+//  Better
+//
+//  Created by Nick Ververis on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
