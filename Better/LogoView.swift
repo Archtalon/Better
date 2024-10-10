769b561298d67858cc8b243322c58ce978bb619f
Nick Ververis <nikolaos.ververis@sword-group.com>
2024-10-10 16:45:24 +0300

    - Adds navigation to OnboardingView and OnboardingView.

--- Better/LogoView.swift
+++ Better/LogoView.swift
@@ -9,63 +9,65 @@ import SwiftUI
 
 struct LogoView: View {
     
-    
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        
-        ZStack {
-            LinearGradient(
-                        gradient: Gradient(stops: [
-                            Gradient.Stop(color:Color("MyWhite"), location: 0.0),   // Start color
-                            Gradient.Stop(color:Color("MyWhite"), location: 0.5),  // Middle color stop
-                            Gradient.Stop(color:Color("MyBlack"), location: 1.0)  // End color 
-                        ]),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
-                    .ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                Spacer()
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color("MyWhite"))
-                        .stroke(Color("MyBlack"), lineWidth:2)
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(stops: [
+                        Gradient.Stop(color:Color("MyWhite"), location: 0.0),   // Start color
+                        Gradient.Stop(color:Color("MyWhite"), location: 0.5),  // Middle color stop
+                        Gradient.Stop(color:Color("MyBlack"), location: 1.0)  // End color
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color("MyWhite"))
+                            .stroke(Color("MyBlack"), lineWidth:2)
+                            .shadow(color: Color("MyBlack").opacity(0.5), radius: 10, x: 5, y: 5)
+                            .frame(width: 150, height: 150)
+                        Text("B")
+                            .font(.system(size:128))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("MyBlack"))
+                            .multilineTextAlignment(.center)
+                    }
+                    Text("Better")
+                        .font(.system(size:96))
+                        .fontWeight(.bold)
                         .shadow(color: Color("MyBlack").opacity(0.5), radius: 10, x: 5, y: 5)
-                        .frame(width: 150, height: 150)
-                    Text("B")
-                        .font(.system(size:128))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("MyBlack"))
-                        .multilineTextAlignment(.center)
-                }
-                Text("Better")
-                    .font(.system(size:96))
-                    .fontWeight(.bold)
+                    HStack {
+                        Text("simple /")
+                        Text("better")
+                            .fontWeight(.bold)
+                        Text("/ efficient")
+                    }
+                    .font(.system(size:12))
                     .shadow(color: Color("MyBlack").opacity(0.5), radius: 10, x: 5, y: 5)
-                HStack {
-                    Text("simple /")
-                    Text("better")
-                        .fontWeight(.bold)
-                    Text("/ efficient")
-                }
-                .font(.system(size:12))
-                .shadow(color: Color("MyBlack").opacity(0.5), radius: 10, x: 5, y: 5)
                     
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                }
                 
-                Spacer()
-                Spacer()
-                Spacer()
-                
+                // NavigationLink to trigger the navigation when tapping anywhere
+                NavigationLink(destination: OnboardingView()) {
+                    Color.clear // Invisible full-page button
+                }
+                .ignoresSafeArea()
             }
+            .colorScheme(.light)
         }
-        .colorScheme(.light)
-        
     }
 }
-    
 
 #Preview {
     LogoView()
---
+++ Better/OnboardingView.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingView.swift
+//  Better
+//
+//  Created by Nick Ververis on 10/10/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    Gradient.Stop(color:Color("MyWhite"), location: 0.0),   // Start color
+                    Gradient.Stop(color:Color("MyWhite"), location: 0.5),  // Middle color stop
+                    Gradient.Stop(color:Color("MyBlack"), location: 1.0)  // End color
+                ]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            Text("Here in Better, we believe that the most valuable resource we have is time. How we choose to spend our time shapes our skills, strengths, ideas and capabilities.  Most of us have it better in one area and we’re  struggling in another. Ultimately, how we separate our days, hours, minutes creates the spectrum of our skillset.  Some of us may need to be better professionals, better artists, better athletes, better friends and partners or a combination of all of the above. Let’s explore together through a series of questions, how much time is available on each one of your days, what are your targets and what we can achieve with it.  Through the advice of experts in each field, we will set what your current level is and create step to step time framed guidelines to advance on your chosen areas.  On each one of your chosen target areas, we will set realistic milestones, end targets and steps to achieve them.  In the end, you will have a complete timeline for your next weeks, months etc.")
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+    
+}
+    
+
+#Preview {
+    OnboardingView()
+}
