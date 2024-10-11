b878f6fcd6d52dca466c947fb187670542f7025b
Nick Ververis <nikolaos.ververis@sword-group.com>
2024-10-11 17:55:48 +0300

    -Adds UserInputView basic content.

--- Better/Views/UserInputView.swift
+++ Better/Views/UserInputView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct UserInputView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationBarBackButtonHidden(true)
+        
+        ZStack(alignment: .top) {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    Gradient.Stop(color:Color("MyWhite"), location: 0.0),   // Start color
+                    Gradient.Stop(color:Color("MyWhite"), location: 0.5),  // Middle color stop
+                    Gradient.Stop(color:Color("MyBlack"), location: 1.0)  // End color
+                ]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            VStack {
+                TopBarView()
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .navigationBarBackButtonHidden(true)
+                    .foregroundColor(Color("MyBlack"))
+                Spacer()
+            }
+            //Spacer()
+            //TopBarView()
+        }
     }
     
 }
