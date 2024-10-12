8fbc7a5212f64670a724eabc6c6dd7763eb4c502
Nick Ververis <nikolaos.ververis@sword-group.com>
2024-10-12 18:23:55 +0300

    -Changes text color on OnboardingView.

--- Better/Views/OnboardingView.swift
+++ Better/Views/OnboardingView.swift
@@ -18,12 +18,21 @@ struct OnboardingView: View {
                     VStack {
                         // Your scrollable content
                         Text("""
+                        
                         Here in Better, we believe that the most valuable resource we have is time. How we choose to spend our time shapes our skills, strengths, ideas and capabilities.
                         
                         Most of us have it better in one area and we’re struggling in another. Ultimately, how we separate our days, hours, minutes creates the spectrum of our skillset.
                         
                         Some of us may need to be better professionals, better artists, better athletes, better friends and partners, or a combination of all of the above. Let’s explore together through a series of questions, how much time is available on each one of your days, what are your targets and what we can achieve with it.
                         
+                        """)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("MyBlack"))
+                        .padding(.top, 90)
+                        .padding(.horizontal, 20)
+                        
+                        
+                        Text("""
                         Through the advice of experts in each field, we will set what your current level is and create step to step time framed guidelines to advance on your chosen areas.
                         
                         On each one of your chosen target areas, we will set realistic milestones, end targets and steps to achieve them.
@@ -31,8 +40,7 @@ struct OnboardingView: View {
                         In the end, you will have a complete timeline for your next weeks, months etc.
                         """)
                         .fontWeight(.bold)
-                        .foregroundColor(Color("MyBlack"))
-                        .padding(.top, 90)
+                        .foregroundColor(Color("MyWhite"))
                         .padding(.horizontal, 20)
                     }
                 }
